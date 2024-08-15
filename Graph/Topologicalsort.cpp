0ab07a7f12e642c314c4c83de7e402406a2aeff6
ashishtad <ashishtad@gmail.com>
2024-08-15 14:11:40 +0530

    Dijkstra Algo Implementation

--- Graph/Topologicalsort.cpp
+++ Graph/Topologicalsort.cpp
@@ -1,70 +1,88 @@
-
-#include "iostream"
-#include<vector>
-#include<queue>
+#include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-class  Graph
-{
+class Graph {
 private:
-    /* data */
     int V;
     vector<vector<int>> adjacency_list;
+
 public:
-     Graph(/* args */);
-    ~ Graph();
+    Graph(int vertices) {
+        V = vertices;
+        adjacency_list.resize(V);
+    }
+
+    ~Graph() {}
+
+    void addEdge(int u, int v) {
+        adjacency_list[u].push_back(v);
+    }
 
     vector<int> topologicalSort() {
-
-        vector<int> vertices_indegree(V,0);
-        //calculate the indegree of each vertex
-        for( int i=0; i<V; ++i) {
-            for( auto neighbour_vertex: adjacency_list[i] )
-                vertices_indegree[neighbour_vertex]++;
+        vector<int> vertices_indegree(V, 0);
+        
+        // Calculate indegrees of all vertices
+        for (int i = 0; i < V; ++i) {
+            for (int neighbor : adjacency_list[i]) {
+                vertices_indegree[neighbor]++;
+            }
         }
 
         queue<int> gq;
-        //Get the vertices with 0 indegress in a queue
-        for( auto v: vertices_indegree) {
-            if( v ==0 )
-                gq.push(v);
+        // Push all vertices with 0 indegree
+        for (int i = 0; i < V; ++i) {
+            if (vertices_indegree[i] == 0)
+                gq.push(i);
         }
-        //Still if queue is empty then start from any node
-        if( gq.empty())
-            gq.push(0); //Here starting with 0
-        
-        vector<int> result;
 
-        while( !gq.empty()) {
+        vector<int> result;
+        while (!gq.empty()) {
             int front = gq.front();
             gq.pop();
-
             result.push_back(front);
 
-            for( auto it : adjacency_list[front]) {
-                //Decrement the neighbour vertx indegree as the starting vertex i.e front is processed.
-                --vertices_indegree[it];
-                if( 0 == vertices_indegree[it])
-                    gq.push(it);
+            for (int neighbor : adjacency_list[front]) {
+                --vertices_indegree[neighbor]; // Reduce indegree
+                if (vertices_indegree[neighbor] == 0)
+                    gq.push(neighbor);
             }
-
         }
-        return result;
 
+        // Cycle detection
+        if (result.size() != V) {
+            cout << "Graph contains a cycle\n";
+            return {};
+        }
+
+        return result;
     }
 
     void printTopologicalSort() {
         vector<int> res = topologicalSort();
+        
+        if (res.empty()) {
+            cout << "Topological sorting is not possible (cycle detected)\n";
+            return;
+        }
 
-        if( res.size() != V)
-            cout<<"Graph contains a cycle\n";
-            
-        for( auto it: res)
-            cout<<it<<"-->";
-
-        cout<<"NULL";
+        for (int v : res)
+            cout << v << " --> ";
+        cout << "NULL\n";
     }
 };
 
- 
+int main() {
+    Graph g(6);
+    g.addEdge(5, 2);
+    g.addEdge(5, 0);
+    g.addEdge(4, 0);
+    g.addEdge(4, 1);
+    g.addEdge(2, 3);
+    g.addEdge(3, 1);
+
+    g.printTopologicalSort();
+    return 0;
+}
